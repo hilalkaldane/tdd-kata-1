e5257d29a05c9333d3434e3990fcd64bb5481266
Hilal Kaldane <hilalkaldane.kaldane937@gmail.com>
2021-01-13 02:16:08 +0530

    test for multiple delimiters

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,15 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
 	public static int add(String numbers) {
-		if(numbers.isEmpty())
-		{
+		if (numbers.isEmpty()) {
 			return 0;
 		}
-		else
-		{
+		else {
 			String[] numbersArray = nonEmptyAddition(numbers);
 			checkForNegatives(numbersArray);
 			return sumOfNumbers(numbersArray);
@@ -17,22 +17,24 @@ public class Calculator {
 	}
 
 	private static void checkForNegatives(String[] numbersArray) {
-		int[] negativeElements=Arrays.stream(numbersArray).parallel().mapToInt(Integer::valueOf).filter(x->x<0).toArray();
-		if(negativeElements.length>0)
-		{
+		int[] negativeElements = Arrays.stream(numbersArray).parallel().mapToInt(Integer::valueOf).filter(x -> x < 0).toArray();
+		if (negativeElements.length > 0) {
 
 			StringBuilder errorMessage = new StringBuilder("negatives not allowed - ");
-			for (int i = 0; i < negativeElements.length-1; i++) {
+			for (int i = 0; i < negativeElements.length - 1; i++) {
 				errorMessage.append(negativeElements[i]);
 				errorMessage.append(",");
 			}
-			errorMessage.append(negativeElements[negativeElements.length-1]);
+			errorMessage.append(negativeElements[negativeElements.length - 1]);
 			throw new RuntimeException(String.valueOf(errorMessage));
 		}
 	}
 
 	private static int sumOfNumbers(String[] numbersArray) {
-		return Arrays.stream(numbersArray).mapToInt(Integer::valueOf).sum();
+		return Arrays.stream(numbersArray)
+				.mapToInt(Integer::valueOf)
+				.filter(n -> n <= 1000)
+				.sum();
 	}
 
 	private static String[] nonEmptyAddition(String numbers) {
@@ -42,8 +44,36 @@ public class Calculator {
 			Pattern pattern = Pattern.compile("//(.*)\n(.*)");
 			Matcher matcher = pattern.matcher(numbers);
 			matcher.find();
-			String delimiter=matcher.group(1);
-			numbers=matcher.group(2);
+			String delimiter = matcher.group(1);
+			if(delimiter.matches("\\[.*\\]"))
+			{
+
+				List<String> delimitersList=new ArrayList<>();
+				int loc=0;
+				while(loc<delimiter.length())
+				{
+					if(delimiter.charAt(loc)=='[')
+					{
+
+						loc++;
+						StringBuilder internalDel=new StringBuilder("(");
+						while(delimiter.charAt(loc)!=']') {
+							internalDel.append("\\").append(delimiter.charAt(loc++));
+						}
+						delimitersList.add(internalDel.append(")").toString());
+						loc++;
+					}
+				}
+				if(delimitersList.size()!=0)
+				{
+					delimiter="";
+				}
+				for (String s : delimitersList) {
+					delimiter += s + "|";
+				}
+				delimiter=delimiter.substring(0,delimiter.length()-1);
+			}
+			numbers = matcher.group(2);
 			numbersArray = numbers.split(delimiter);
 		}
 		else {
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -5,50 +5,49 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestCalculator {
 	@Test
-	public void shouldReturnZeroIfEmpty()
-	{
+	public void shouldReturnZeroIfEmpty() {
 		try {
-			assertEquals(0,Calculator.add(""));
+			assertEquals(0, Calculator.add(""));
 		}
 		catch (Exception e) {
 			assertNull(e);
 		}
 	}
+
 	@Test
-	public void shouldReturnNumber()
-	{
+	public void shouldReturnNumber() {
 		try {
-			assertEquals(1,Calculator.add("1"));
+			assertEquals(1, Calculator.add("1"));
 		}
 		catch (Exception e) {
 			assertNull(e);
 		}
 	}
+
 	@Test
-	public void shouldReturnSumOf2NumbersDelimitedByComma()
-	{
+	public void shouldReturnSumOf2NumbersDelimitedByComma() {
 		try {
-			assertEquals(3,Calculator.add("1,2"));
+			assertEquals(3, Calculator.add("1,2"));
 		}
 		catch (Exception e) {
 			assertNull(e);
 		}
 	}
+
 	@Test
-	public void shouldReturnSumOfNumbersOfAnyCountDelimitedByComma()
-	{
+	public void shouldReturnSumOfNumbersOfAnyCountDelimitedByComma() {
 		try {
-			assertEquals(6,Calculator.add("1,2,3"));
+			assertEquals(6, Calculator.add("1,2,3"));
 		}
 		catch (Exception e) {
 			assertNull(e);
 		}
 	}
+
 	@Test
-	public void shouldReturnSumDelimitedByCommaAndNewLine()
-	{
+	public void shouldReturnSumDelimitedByCommaAndNewLine() {
 		try {
-			assertEquals(6,Calculator.add("1\n2,3"));
+			assertEquals(6, Calculator.add("1\n2,3"));
 		}
 		catch (Exception e) {
 			assertNull(e);
@@ -57,24 +56,43 @@ public class TestCalculator {
 				assertEquals(6,Calculator.add("1,\n2,3"));
 		 */
 	}
+
 	@Test
-	public void shouldReturnSumWithInputGivenWithSpecifiedDelimiter()
-	{
+	public void shouldReturnSumWithInputGivenWithSpecifiedDelimiter() {
 		try {
-			assertEquals(3,Calculator.add("//;\n1;2"));
+			assertEquals(3, Calculator.add("//;\n1;2"));
+		}
+		catch (Exception e) {
+			assertNull(e);
+		}
+	}
+
+	@Test
+	public void shouldThrowExceptionForNegative() {
+		try {
+			Calculator.add("//;\n-1;2");
+		}
+		catch (Exception e) {
+			assertEquals("negatives not allowed - -1", e.getMessage());
+		}
+	}
+
+	@Test
+	public void shouldCheckForNumberSmallerThan1001() {
+		try {
+			assertEquals(2,Calculator.add("//;\n1001;2"));
 		}
 		catch (Exception e) {
 			assertNull(e);
 		}
 	}
 	@Test
-	public void shouldThrowExceptionForNegative()
-	{
+	public void checkForMultipleDelimeters() {
 		try {
-			Calculator.add("//;\n-1;2");
+			assertEquals(6,Calculator.add("//[*][%]\n1*2%3"));
 		}
 		catch (Exception e) {
-			assertEquals("negatives not allowed - -1",e.getMessage());
+			assertNull(e);
 		}
 	}
 }
