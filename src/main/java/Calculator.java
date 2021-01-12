97d5377c2a42ffe1e663e00a3b104c50f56bb2de
Hilal Kaldane <hilalkaldane.kaldane937@gmail.com>
2021-01-13 02:27:30 +0530

    refactors code and final commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,15 +11,64 @@ public class Calculator {
 		}
 		else {
 			String[] numbersArray = nonEmptyAddition(numbers);
+			//if negative exists, throws error.
 			checkForNegatives(numbersArray);
+			//else returns sum of error
 			return sumOfNumbers(numbersArray);
 		}
+	}
+
+	private static String[] nonEmptyAddition(String numbers) {
+		String[] numbersArray;
+		//check for custom delimiter existence
+		if (numbers.startsWith("//")) {
+			//splits numbers based on delimiter provided
+			Pattern pattern = Pattern.compile("//(.*)\n(.*)");
+			Matcher matcher = pattern.matcher(numbers);
+			matcher.find();
+			String delimiter = matcher.group(1);
+			//checks if multiple delimiters are provided.
+			if (delimiter.matches("\\[.*\\]")) {
+				delimiter = createMultipleDelimiterArray(delimiter);
+			}
+			numbers = matcher.group(2);
+			numbersArray = numbers.split(delimiter);
+		}
+		else {
+			//splits input by delimiter as , and \n
+			numbersArray = numbers.split(",|\\n");
+		}
+		return numbersArray;
+	}
+
+	private static String createMultipleDelimiterArray(String delimiter) {
+		List<String> delimitersList = new ArrayList<>();
+		int loc = 0;
+		while (loc < delimiter.length()) {
+			//start of delimiter
+			if (delimiter.charAt(loc) == '[') {
+				loc++;
+				StringBuilder internalDelimiter = new StringBuilder("(");
+				while (delimiter.charAt(loc) != ']') {
+					internalDelimiter.append("\\").append(delimiter.charAt(loc++));
+				}
+				delimitersList.add(internalDelimiter.append(")").toString());
+				//end of delimiter
+				loc++;
+			}
+		}
+		if (delimitersList.size() != 0) {
+			delimiter = "";
+		}
+		for (String s : delimitersList) {
+			delimiter += s + "|";
+		}
+		return delimiter.substring(0, delimiter.length() - 1);
 	}
 
 	private static void checkForNegatives(String[] numbersArray) {
 		int[] negativeElements = Arrays.stream(numbersArray).parallel().mapToInt(Integer::valueOf).filter(x -> x < 0).toArray();
 		if (negativeElements.length > 0) {
-
 			StringBuilder errorMessage = new StringBuilder("negatives not allowed - ");
 			for (int i = 0; i < negativeElements.length - 1; i++) {
 				errorMessage.append(negativeElements[i]);
@@ -31,55 +80,10 @@ public class Calculator {
 	}
 
 	private static int sumOfNumbers(String[] numbersArray) {
+		//ignores number greater than 1000 and returns sum
 		return Arrays.stream(numbersArray)
 				.mapToInt(Integer::valueOf)
 				.filter(n -> n <= 1000)
 				.sum();
-	}
-
-	private static String[] nonEmptyAddition(String numbers) {
-		String[] numbersArray;
-		if (numbers.startsWith("//")) {
-			//splits numbers based on delimiter provided
-			Pattern pattern = Pattern.compile("//(.*)\n(.*)");
-			Matcher matcher = pattern.matcher(numbers);
-			matcher.find();
-			String delimiter = matcher.group(1);
-			if(delimiter.matches("\\[.*\\]"))
-			{
-
-				List<String> delimitersList=new ArrayList<>();
-				int loc=0;
-				while(loc<delimiter.length())
-				{
-					if(delimiter.charAt(loc)=='[')
-					{
-
-						loc++;
-						StringBuilder internalDel=new StringBuilder("(");
-						while(delimiter.charAt(loc)!=']') {
-							internalDel.append("\\").append(delimiter.charAt(loc++));
-						}
-						delimitersList.add(internalDel.append(")").toString());
-						loc++;
-					}
-				}
-				if(delimitersList.size()!=0)
-				{
-					delimiter="";
-				}
-				for (String s : delimitersList) {
-					delimiter += s + "|";
-				}
-				delimiter=delimiter.substring(0,delimiter.length()-1);
-			}
-			numbers = matcher.group(2);
-			numbersArray = numbers.split(delimiter);
-		}
-		else {
-			//splits input by delimiter as , and \n
-			numbersArray = numbers.split(",|\\n");
-		}
-		return numbersArray;
 	}
 }
